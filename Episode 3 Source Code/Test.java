09c0184c9971896c45f6443ef09888b9e13bbb47
Azim Python <evozims@users.noreply.github.com>
2017-10-15 05:09:36 -0700

    Update Test.java

--- Episode 3 Source Code/Test.java
+++ Episode 3 Source Code/Test.java
@@ -2,6 +2,21 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		int x = 1000000;
+		int y = 2000000;
+		
+		int sum = x + y;
+		int difference = x - y; 
+		int product = x * y;
+		int quotient = x / y;
+		
+		System.out.println(sum);
+		System.out.println(difference);
+		System.out.println(product);
+		System.out.println(quotient);
+		
+		//System.out.println(x);
+		//System.out.println(y);
+
 	}
 }
