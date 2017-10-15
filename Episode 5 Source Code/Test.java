3df2e18c50f91ce86a13d99ab3ac3fe1ee41c0e5
Azim Python <evozims@users.noreply.github.com>
2017-10-15 05:10:53 -0700

    Update Test.java

--- Episode 5 Source Code/Test.java
+++ Episode 5 Source Code/Test.java
@@ -1,8 +1,35 @@
-public class Test {
+package demo;
+
+/* Author: EvoZims
+ * Description: Java Program - Price Calculator
+ * This program will be able to calculate how much an item will cost.
+ * Date: 10/15/2017
+ * 
+ * Updates: 
+ * Version: 1.0.0
+ * 
+ * Product: iPhone X - $1000
+ * Tax: 9%
+ * 
+ * How much money would I have to pay, if I bought 5 iPhone Xs. 
+ * 
+ */
+
+public class PriceCalculator {
 	
 	public static void main(String[] args) {
-
-		System.out.println("There's nothing in here.");
-
+		
+		int iPhone = 1000;
+		double taxRate = 9;
+		
+		double taxRateInDecimal = taxRate / 100;
+		double totalTax = taxRateInDecimal * iPhone; 
+	
+		double totalPrice = totalTax + iPhone;
+		double totalPriceOfFiveIphones = totalPrice * 5;
+		
+		System.out.println(totalPriceOfFiveIphones);
+		
 	}
+	
 }
